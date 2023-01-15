1f26d3490720540eac4089665c8236d662435707
Arafat Alim <Arafat97alim@gmail.com>
2023-01-15 19:01:37 +0530

    ✨ Updated the Site Icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  //! creating state
+  const [text, setText] = useState("");
+
+  //! Methods
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  const startGame = (e) => {};
+
   return (
     <form>
       <h1>How fast Do you Type? 🔥</h1>
-      <textarea />
+      <textarea value={text} onChange={handleChange} />
       <h4>Time Remaining: ???</h4>
       <button>Start</button>
       <h1>Word Count: ???</h1>
